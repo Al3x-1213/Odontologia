721c61b1120a6db3e12ee11d43fa4455fa61f94f
Alex <alejandrourdaneta1213@hotmail.com>
2023-05-21 10:21:28 -0400

    Se eliminaron los dos formularios para dejar el formulario de login generico

--- client/validacion.php
+++ client/validacion.php
@@ -1,7 +1,7 @@
 <?php
 
 /* DATOS RECIBIDOS DEL LOGUEO PARA SER VALIDADOS DENTRO DE LA BASE DE DATOS */
-$correo=$_POST['correo'];
+$nombre_usuario=$_POST['nombre_usuario'];
 $clave=$_POST['clave'];
 
 /* ESTO ES PARA VARIABLES GLOBALES  */
@@ -10,24 +10,24 @@ ob_start();
 $_SESSION['sesion'] = 0;
 
 /* VALIDAR QUE LOS DATOS NO ESTÉN VACIOS  */
-if($correo == '' || $clave == ''){
+if($nombre_usuario == '' || $clave == ''){
     header('Location:../index.php');
 }else{
     $clave=md5($clave);
 
     include 'conexion.php';
 
-    $validar = "SELECT * FROM paciente1 WHERE correo='$correo'";
+    $validar = "SELECT * FROM admin WHERE nombre_usuario='$nombre_usuario'";
     $validando = $conexion->query($validar);
 
     $respuesta = mysqli_fetch_array($validando);
 
     if($respuesta['clave'] != $clave){
         mysqli_close($conexion);
-        ?> <script> alert("Correo o contraseña equivocado") </script> <?php
+        ?> <script> alert("nombre_usuario o contraseña equivocado") </script> <?php
         header('Location:../parts/login.html');
     }else{
-        $validar = "SELECT * FROM paciente1 WHERE correo='$correo' AND clave='$clave'";
+        $validar = "SELECT * FROM admin WHERE nombre_usuario='$nombre_usuario' AND clave='$clave'";
         $validando = $conexion->query($validar);
         if($validando->num_rows > 0){
             /* CONSULTA A LA BASE DE DATOS PARA VER SI EL USUARIO ESTÁ AHÍ */
@@ -35,7 +35,7 @@ if($correo == '' || $clave == ''){
     
             /* CERRAR LA CONSULTA DE LA BASE DE DATOS */
             mysqli_close($conexion);
-            header('Location:../paciente/index.php');
+            header('Location:../admin/index.php');
         }else{
             mysqli_close($conexion);
             header('Location:../index.php');
