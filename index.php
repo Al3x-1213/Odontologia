cc4e055cc17ba9bbc140dc654450b82d36a2407e
Alex <alejandrourdaneta1213@hotmail.com>
2023-06-05 19:06:11 -0400

    mejorada la vista principal

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $_SESSION['sesion'] = 0;
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Raleway:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
-  <title>Consultorio Riccio</title>
+  <title>Consultorio Mirasol Díaz</title>
 </head>
 
 <body>
