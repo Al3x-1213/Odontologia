30be231aa0b4d0c1aab2788b7f8e892a57a6bd1b
Alex <alejandrourdaneta1213@hotmail.com>
2023-06-07 00:27:21 -0400

    no se x2

---
+++ js/validacionRegistrarse.js
@@ -0,0 +1,16 @@
+const boton = document.querySelector(".button");
+
+//DATOS DE USUARIO
+const usuario = document.querySelector(".usuario");
+const clave = document.querySelector(".clave");
+const clave2 = document.querySelector(".clave2");
+
+//DATOS PERSONALES
+const nombre = document.querySelector(".nombre");
+const apellido = document.querySelector(".apellido");
+const cedula = document.querySelector(".cedula");
+const nacimiento = document.querySelector(".nacimiento");
+const telefono1 = document.querySelector(".telefono1");
+const telefono2 = document.querySelector(".telefono2");
+const correo = document.querySelector(".correo");
+
--- parts/registrarse.php
+++ parts/registrarse.php
@@ -36,43 +36,45 @@
                     <h3>Tu cuenta: </h3>
 
                     <label for="nombre">Usuario:</label>
-                    <input type="text" maxlength="30" required="true" name="usuario" class="input__form" autocomplete="off">
+                    <input type="text" maxlength="30" required="true" name="usuario" class="input__form usuario" autocomplete="off">
 
                     <label for="clave">Contraseña:</label>
-                    <input type="password" maxlength="35" required="true" name="clave" class="input__form">
+                    <input type="password" maxlength="35" required="true" name="clave" class="input__form clave">
 
                     <label for="clave">Confirmar Contraseña:</label>
-                    <input type="password" maxlength="35" required="true" name="clave2" class="input__form">
+                    <input type="password" maxlength="35" required="true" name="clave2" class="input__form clave2">
 
                     <h3>Datos Personales: </h3>
 
                     <label for="nombre">Nombre:</label>
-                    <input type="text" maxlength="25" required="true" name="nombre" class="input__form" autocomplete="off">
+                    <input type="text" maxlength="25" required="true" name="nombre" class="input__form nombre" autocomplete="off">
 
                     <label for="apellido">Apellido:</label>
-                    <input type="text" maxlength="25" required="true" name="apellido" class="input__form">
+                    <input type="text" maxlength="25" required="true" name="apellido" class="input__form apellido">
 
                     <label for="cedula">Cédula:</label>
-                    <input type="number" maxlength="8" required="true" name="cedula" class="input__form">
+                    <input type="number" maxlength="8" required="true" name="cedula" class="input__form cedula">
 
                     <?php
                     $limiteFecha = date("Y-m-d"); 
                     ?>
                     <label for="edad">Fecha de Nacimiento:</label>
-                    <input type="date" required="true" name="nacimiento" max="<?= $limiteFecha; ?>" class="input__form">
+                    <input type="date" required="true" name="nacimiento" max="<?= $limiteFecha; ?>" class="input__form nacimiento">
 
                     <label for="numero">Teléfono Celular: </label>
-                    <input type="number" maxlength="11" required="true" name="telefono1" class="input__form">
+                    <input type="number" maxlength="11" required="true" name="telefono1" class="input__form telefono1">
 
                     <label for="numero">Teléfono (opcional):</label>
-                    <input type="number" maxlength="11" name="telefono2" class="input__form">
+                    <input type="number" maxlength="11" name="telefono2" class="input__form telefono2">
 
                     <label for="correo">Correo Electrónico:</label>
-                    <input type="email" maxlength="60" required="true" name="correo" class="input__form" autocomplete="off">
+                    <input type="email" maxlength="60" required="true" name="correo" class="input__form correo" autocomplete="off">
 
                     <input class="button" type="submit" value="Registrarse" name="boton_reg">
                 </div>
             </form>
         </div>
     </body>
+
+    <script src="../js/validacionRegistrarse.js"></script>
 </html>
