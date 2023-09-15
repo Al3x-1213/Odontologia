e591b27c31ae271489ba3003f434d7379eed1f13
VeronicaTrias <gitygithub@gmail.com>
2023-09-14 22:17:15 -0400

    buscador exclusivo para pacientes

--- admin/searchPatientsFilter.php
+++ admin/searchPatientsFilter.php
@@ -15,8 +15,34 @@ $query->execute([$buscar . '%', $buscar . '%', $buscar . '%']);
 
 $contenido = "";
 
-while ($resultado = $query->fetch(PDO::FETCH_ASSOC)) {
-    $contenido .= "<li><a href='searchPatients.php?id=".$resultado['id_dato_personal']."'>". $resultado["nombre"]. " ". $resultado["apellido"]. "<span class='cedulaSearch'> (".  $resultado["cedula"]. ") </span>". "</a></li>";
+while ($resultado = $query->fetch(PDO::FETCH_ASSOC)){
+    $idDatoPersonal = $resultado['id_dato_personal'];
+
+    include '../client/connection.php';
+
+    $consulta2 = "SELECT id_cuenta FROM cuentas WHERE id_dato_personal = '$idDatoPersonal'";
+    $query2 = mysqli_query($conexion, $consulta2);
+        
+    $resultado2= mysqli_num_rows($query2);
+
+    if ($resultado2 != 0){
+        $respuesta = mysqli_fetch_array($query2);
+        $idCuenta = $respuesta['id_cuenta'];
+
+        $consulta2 = "SELECT id_doctor FROM doctores WHERE id_usuario = '$idCuenta'";
+        $query2 = mysqli_query($conexion, $consulta2);
+
+        $resultado2= mysqli_num_rows($query2);
+
+        if ($resultado2 == 0){
+            $contenido .= "<li><a href='searchPatients.php?id=".$resultado['id_dato_personal']."'>". $resultado["nombre"]. " ". $resultado["apellido"]. "<span class='cedulaSearch'> (".  $resultado["cedula"]. ") </span>". "</a></li>";
+        }
+    }
+    else{
+        $contenido .= "<li><a href='searchPatients.php?id=".$resultado['id_dato_personal']."'>". $resultado["nombre"]. " ". $resultado["apellido"]. "<span class='cedulaSearch'> (".  $resultado["cedula"]. ") </span>". "</a></li>";
+    }
+
+    // $contenido .= "<li><a href='searchPatients.php?id=".$resultado['id_dato_personal']."'>". $resultado["nombre"]. " ". $resultado["apellido"]. "<span class='cedulaSearch'> (".  $resultado["cedula"]. ") </span>". "</a></li>";
 }
 
 echo json_encode($contenido, JSON_UNESCAPED_UNICODE);
