abf9f5c2c5f420231931ef830b517303df7cd23e
VeronicaTrias <gitygithub@gmail.com>
2023-09-17 22:10:25 -0400

    constancia medica

--- client/botones/printConstancy.php
+++ client/botones/printConstancy.php
@@ -41,39 +41,29 @@ $html = '
     </head>
     <body>
         <header>
-            <div class="encabezado">
-                <div class="imagen">
-                    <img src="../../img/documento.png">
-                </div>
-                <div class="informacion">
-                    <p class="linea1">Dra. Marisol Díaz Aira</p>
-                    <p class="linea2">Odontólogo / Estética - Protesis</p>
-                    <p class="linea3">odontologiamarisoldiaz@gmail.com</p>
-                </div>
-            </div>
+            <p class="linea1">Dra. Marisol Díaz Aira</p>
+            <p class="linea2">Odontólogo / Estética - Protesis</p>
+            <p class="linea3">odontologiamarisoldiaz@gmail.com</p>
         </header>
 
-        <div class="franja">
-            `
-        </div>
+        <p class="franja">____________________________________________________________________________________________________</p>
 
         <div class="cuerpo">
-            <h1>CONSTANCIA MÉDICA</h1>
+            <h2>CONSTANCIA MÉDICA</h2>
 
             <div class="contenido">
-            <p class="parrafo1">Por medio de la presente hago constar que el paciente '. '<b>'. $nombre. ' '. $apellido. '</b>'. ' de '. '<b>'. $edad. ' años'. '</b>'. ' de edad, portador de la cédula de identidad '. '<b>'.  'V-'. $cedula. '</b>'. ', asistió a una cita odontológica el día '. '<b>'. $diaSemana. ' '. $day. ' de '. $mesAño. ' de '. $year. '</b>'. '.'. '</p>
-            <p class="parrafo2">Constancia que expide a petición de la parte interesada.</p>
+                <p class="parrafo1">Por medio de la presente hago constar que el paciente '. '<b>'. $nombre. ' '. $apellido. '</b>'. ' de '. '<b>'. $edad. ' años'. '</b>'. ' de edad, portador de la cédula de identidad '. '<b>'.  'V-'. $cedula. '</b>'. ', asistió a una cita odontológica el día '. '<b>'. $diaSemana. ' '. $day. ' de '. $mesAño. ' de '. $year. '</b>'. '.'. '</p>
+                <br><p class="parrafo2">Constancia que expide a petición de la parte interesada.</p>
             </div>
 
             <div class="atentamente">
-                <p class="linea1">Atentamente</p>
+                <p class="linea1">______________________________</p>
+                <p class="linea2">Atentamente</p>
                 <p class="linea2">Dra. Marisol Díaz</p>
             </div>
         </div>
 
-        <div class="franja">
-            `
-        </div>
+        <p class="franja">____________________________________________________________________________________________________</p>
 
         <div class="pie">
             <p class="linea1">Av. Francisco de Miranda, Edif. Lucerna - PB / #4. Chacao, Caracas</p>
@@ -83,6 +73,7 @@ $html = '
 </html>
 ';
 
+$domPdf->set_paper('letter', 'landscape');
 $domPdf->load_html($html);
 $domPdf->render();
 $domPdf->stream("documento.pdf", array('Attachment' => '0'));
