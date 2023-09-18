58d49c98b592d90aad94d009c1179e60f2433043
Alex <alejandrourdaneta1213@hotmail.com>
2023-09-18 18:01:39 -0400

    responsive admin parte2

--- admin/responsive/header.php
+++ admin/responsive/header.php
@@ -6,11 +6,11 @@
     <nav>
         <ul class="list__menu-responsive">
             <a href="" class="item__menu-responsive"><li>Sistema de control de Citas</li><i class="icon-paste"></i></a>
-            <a href="" class="item__menu-responsive"><li>Registrar Citas</li><i class="icon-pencil"></i></a>
             <a href="" class="item__menu-responsive"><li>Confirmar Citas</li><i class="icon-checkmark1"></i></a>
             <a href="" class="item__menu-responsive"><li>Citas Atendidas</li><i class="icon-folder"></i></a>
-            <a href="" class="item__menu-responsive"><li>Usuarios Registrados</li><i class="icon-address-book"></i></a>
+            <a href="registeredUser.php" class="item__menu-responsive"><li>Usuarios Registrados</li><i class="icon-address-book"></i></a>
             <a href="" class="item__menu-responsive"><li>Registrar Usuario</li><i class="icon-user"></i></a>
+            <a href="../client/logout.php" class="item__menu-responsive"><li>Cerrar Sesión</li></a>
         </ul>
     </nav>
 </div>
