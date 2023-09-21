31f269aa01c3a34deae42d0d1e4d2e6976a92061
VeronicaTrias <gitygithub@gmail.com>
2023-09-20 22:47:31 -0400

    evitar solicitudes de consultas repetidas desde la vista de la doctora

--- client/insert/registerCita.php
+++ client/insert/registerCita.php
@@ -16,24 +16,45 @@ if (!empty($_POST['boton_c'])){
         $idDoctor = $_POST['id_doctor'];
         $idStatusConsulta = 3;
 
+        // COMPROBAR QUE EL PACIENTE NO TENGA UNA CITA POR ATENDER
         include '../connection.php'; //Conexión con base de datos
 
-        // INGRESAR LA CONSULTA A BASE DE DATOS
-        $consulta = "INSERT INTO consultas (id_consulta, id_paciente, id_causa_consulta, fecha_atencion, id_turno_consulta, hora_inicio,
-        hora_fin, id_doctor, id_status_consulta, fecha_solicitud) VALUES (NULL, '$idPaciente', '$causa', '$fechaAtencion', '$turno',
-        NULL, NULL, '$idDoctor', '$idStatusConsulta', now())";
-        $query = mysqli_query($conexion, $consulta); 
+        $consulta = "SELECT id_consulta FROM consultas WHERE id_paciente = '$idPaciente' AND (id_status_consulta = 3 OR id_status_consulta = 2)";
+        $query = mysqli_query($conexion, $consulta);
 
-        if($query){
+        $resultado= mysqli_num_rows($query);
+
+        if ($resultado == 1){
+            $consulta = "SELECT nombre, apellido FROM datos_personales WHERE id_dato_personal = '$idPaciente'";
+            $query = mysqli_query($conexion, $consulta);
+
+            $respuesta = mysqli_fetch_array($query);
+            $nombre = $respuesta['nombre'];
+            $apellido = $respuesta['apellido'];
+
             ?>
-            <div class= "mensaje"><a href= "#">Cita agendada correctamente</a></div>
+            <div class= "alerta"><a href= "index.php">Solicitud no enviada</a></div>
+            <div class= "alerta"><a href= "index.php"><?php echo $nombre. " ". $apellido; ?> ya tiene una cita por atender</a></div>
             <?php
         }
         else{
-            ?>
-            <div class= "alerta">No se pudo agendar la cita</div>
-            <?php
-         }
+             // INGRESAR LA CONSULTA A BASE DE DATOS
+            $consulta = "INSERT INTO consultas (id_consulta, id_paciente, id_causa_consulta, fecha_atencion, id_turno_consulta, hora_inicio,
+            hora_fin, id_doctor, id_status_consulta, fecha_solicitud) VALUES (NULL, '$idPaciente', '$causa', '$fechaAtencion', '$turno',
+            NULL, NULL, '$idDoctor', '$idStatusConsulta', now())";
+            $query = mysqli_query($conexion, $consulta); 
+
+            if($query){
+                ?>
+                <div class= "mensaje"><a href= "#">Cita agendada correctamente</a></div>
+                <?php
+            }
+            else{
+                ?>
+                <div class= "alerta">No se pudo agendar la cita</div>
+                <?php
+            }
+        }       
     }
 }
 
