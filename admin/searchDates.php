0e553a38c1b860af76647048aa112d6099909d30
VeronicaTrias <gitygithub@gmail.com>
2023-09-21 16:48:29 -0400

    cambios

--- admin/searchDates.php
+++ admin/searchDates.php
@@ -61,93 +61,97 @@ $year = date("Y");
         if($day >= $dia && $month >= $mes && $Year >= $year){
         ?>
             <h2 class="dia"><?php echo ordenarFecha($fechaBuscar); ?></h2>
+            
+            <div class="table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Paciente</th>
+                            <th>Cédula</th>
+                            <th>Edad</th>
+                            <th>Motivo de la Consulta</th>
+                            <th>Hora de Atención</th>
+                            <th>Teléfono</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    // BUSCAR DÍA SELECCIONADO
+                    $consulta = "SELECT * FROM consultas INNER JOIN datos_personales INNER JOIN causa_consulta INNER JOIN doctores INNER JOIN status_consulta
+                    ON consultas.id_paciente = datos_personales.id_dato_personal AND consultas.id_causa_consulta = causa_consulta.id_causa_consulta
+                    AND consultas.id_doctor = doctores.id_doctor AND consultas.id_status_consulta = status_consulta.id_status_consulta
+                    WHERE consultas.id_doctor = '$idDoctor' AND consultas.fecha_atencion = '$fechaBuscar' AND consultas.id_status_consulta != 3
+                    ORDER BY hora_inicio ASC";
+                    $query = mysqli_query($conexion, $consulta);
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Paciente</th>
-                        <th>Cédula</th>
-                        <th>Edad</th>
-                        <th>Motivo de la Consulta</th>
-                        <th>Hora de Atención</th>
-                        <th>Teléfono</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                // BUSCAR DÍA SELECCIONADO
-                $consulta = "SELECT * FROM consultas INNER JOIN datos_personales INNER JOIN causa_consulta INNER JOIN doctores INNER JOIN status_consulta
-                ON consultas.id_paciente = datos_personales.id_dato_personal AND consultas.id_causa_consulta = causa_consulta.id_causa_consulta
-                AND consultas.id_doctor = doctores.id_doctor AND consultas.id_status_consulta = status_consulta.id_status_consulta
-                WHERE consultas.id_doctor = '$idDoctor' AND consultas.fecha_atencion = '$fechaBuscar' AND consultas.id_status_consulta != 3
-                ORDER BY hora_inicio ASC";
-                $query = mysqli_query($conexion, $consulta);
-
-                while ($respuesta = mysqli_fetch_array($query)) {
-                    $horaInicio = date("g:i a",strtotime($respuesta['hora_inicio']));
-                    $horaFin = date("g:i a",strtotime($respuesta['hora_fin']));
-                ?>
-                    <tr>
-                        <td><?php echo $respuesta['nombre']. " ". $respuesta['apellido']; ?></td>
-                        <td><?php echo $respuesta['cedula']; ?></td>
-                        <td><?php echo $respuesta['edad']; ?></td>
-                        <td><?php echo $respuesta['causa_consulta']; ?></td>
-                        <td><?php echo $horaInicio . " - " . $horaFin; ?></td>
-                        <td><?php echo $respuesta['telefono_1'] . "<br>" . $respuesta['telefono_2']; ?></td>
-                        <td>
-                            <a href="../client/botones/cancel.php?id=<?php echo $respuesta['id_consulta'] ?>"><button title="Cancelar" class="cancel"><i class="icon-cross icon"></i></button></a>
-                        </td>
-                    </tr>
-                <?php
-                }
-                ?>
-                </tbody>
-            </table>
+                    while ($respuesta = mysqli_fetch_array($query)) {
+                        $horaInicio = date("g:i a",strtotime($respuesta['hora_inicio']));
+                        $horaFin = date("g:i a",strtotime($respuesta['hora_fin']));
+                    ?>
+                        <tr>
+                            <td><?php echo $respuesta['nombre']. " ". $respuesta['apellido']; ?></td>
+                            <td><?php echo $respuesta['cedula']; ?></td>
+                            <td><?php echo $respuesta['edad']; ?></td>
+                            <td><?php echo $respuesta['causa_consulta']; ?></td>
+                            <td><?php echo $horaInicio . " - " . $horaFin; ?></td>
+                            <td><?php echo $respuesta['telefono_1'] . "<br>" . $respuesta['telefono_2']; ?></td>
+                            <td>
+                                <a href="../client/botones/cancel.php?id=<?php echo $respuesta['id_consulta'] ?>"><button title="Cancelar" class="cancel"><i class="icon-cross icon"></i></button></a>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
         <?php
         }
         else{ 
         ?>
             <h2 class="dia"><?php echo ordenarFecha($fechaBuscar); ?></h2>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Paciente</th>
-                        <th>Cédula</th>
-                        <th>Edad</th>
-                        <th>Motivo de la Consulta</th>
-                        <th>Hora de Atención</th>
-                        <th>Teléfono</th>                        
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                // BUSCAR DÍA SELECCIONADO
-                $consulta = "SELECT * FROM consultas INNER JOIN datos_personales INNER JOIN causa_consulta INNER JOIN doctores INNER JOIN status_consulta
-                ON consultas.id_paciente = datos_personales.id_dato_personal AND consultas.id_causa_consulta = causa_consulta.id_causa_consulta
-                AND consultas.id_doctor = doctores.id_doctor AND consultas.id_status_consulta = status_consulta.id_status_consulta
-                WHERE consultas.id_doctor = '$idDoctor' AND consultas.fecha_atencion = '$fechaBuscar' AND  consultas.id_status_consulta != 4
-                ORDER BY hora_inicio ASC";
-                $query = mysqli_query($conexion, $consulta);
+            <div class="table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Paciente</th>
+                            <th>Cédula</th>
+                            <th>Edad</th>
+                            <th>Motivo de la Consulta</th>
+                            <th>Hora de Atención</th>
+                            <th>Teléfono</th>                        
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    // BUSCAR DÍA SELECCIONADO
+                    $consulta = "SELECT * FROM consultas INNER JOIN datos_personales INNER JOIN causa_consulta INNER JOIN doctores INNER JOIN status_consulta
+                    ON consultas.id_paciente = datos_personales.id_dato_personal AND consultas.id_causa_consulta = causa_consulta.id_causa_consulta
+                    AND consultas.id_doctor = doctores.id_doctor AND consultas.id_status_consulta = status_consulta.id_status_consulta
+                    WHERE consultas.id_doctor = '$idDoctor' AND consultas.fecha_atencion = '$fechaBuscar' AND  consultas.id_status_consulta != 4
+                    ORDER BY hora_inicio ASC";
+                    $query = mysqli_query($conexion, $consulta);
 
-                while ($respuesta = mysqli_fetch_array($query)) {
-                    $horaInicio = date("g:i a",strtotime($respuesta['hora_inicio']));
-                    $horaFin = date("g:i a",strtotime($respuesta['hora_fin']));
-                ?>
-                    <tr>
-                        <td><?php echo $respuesta['nombre']. " ". $respuesta['apellido']; ?></td>
-                        <td><?php echo $respuesta['cedula']; ?></td>
-                        <td><?php echo $respuesta['edad']; ?></td>
-                        <td><?php echo $respuesta['causa_consulta']; ?></td>
-                        <td><?php echo $horaInicio . " - " . $horaFin; ?></td>
-                        <td><?php echo $respuesta['telefono_1'] . "<br>" . $respuesta['telefono_2']; ?></td>                        
-                    </tr>
-                <?php
-                }
-                ?>
-                </tbody>
-            </table>
+                    while ($respuesta = mysqli_fetch_array($query)) {
+                        $horaInicio = date("g:i a",strtotime($respuesta['hora_inicio']));
+                        $horaFin = date("g:i a",strtotime($respuesta['hora_fin']));
+                    ?>
+                        <tr>
+                            <td><?php echo $respuesta['nombre']. " ". $respuesta['apellido']; ?></td>
+                            <td><?php echo $respuesta['cedula']; ?></td>
+                            <td><?php echo $respuesta['edad']; ?></td>
+                            <td><?php echo $respuesta['causa_consulta']; ?></td>
+                            <td><?php echo $horaInicio . " - " . $horaFin; ?></td>
+                            <td><?php echo $respuesta['telefono_1'] . "<br>" . $respuesta['telefono_2']; ?></td>                        
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
         <?php 
         }
         mysqli_close($conexion);
