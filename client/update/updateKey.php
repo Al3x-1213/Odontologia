d807550d5e7c2e9d4e553668ced46735ae1ba8b4
VeronicaTrias <gitygithub@gmail.com>
2023-09-30 18:40:34 -0400

    editar perfil en el apartado de pacientes

--- client/update/updateKey.php
+++ client/update/updateKey.php
@@ -5,7 +5,6 @@ ob_start();
 if (!empty($_POST['button_upd'])){
     // VERIFICAR QUE NO HAYAN CAMPOS VACIOS
     if (empty($_POST['claveAnt']) || empty($_POST['clave']) || empty($_POST['clave2'])){
-        // session_start();
         $_SESSION['mensaje'] = "No deben haber campos vacios";
         $_SESSION['error'] = 1;
         header("location: ../../paciente/editarPerfil/editarClave.php");
@@ -19,15 +18,10 @@ if (!empty($_POST['button_upd'])){
         $clave = $_POST['clave'];
         $claveConfirm = $_POST['clave2'];
 
-        // echo $claveAnt. "<br>";
-        // echo $clave. "<br>";
-        // echo $claveConfirm. "<br>";
-
         // CONFIRMAR QUE LA CONTRASEÑA ANTERIOR SEA CORRECTA
         include '../connection.php'; //Conexión con base de datos
 
         $consulta = "SELECT clave FROM cuentas WHERE id_cuenta = '$id'";
-        // echo $consulta;
         $query = mysqli_query($conexion, $consulta);
 
         $resultado = mysqli_fetch_array($query);
@@ -40,7 +34,6 @@ if (!empty($_POST['button_upd'])){
             header("location: ../../paciente/editarPerfil/editarClave.php");
         }
         else{
-            // echo "si";
             // VERIFICAR QUE AMBAS CONTRASEÑAS SEAN IGUALES
             if ($clave != $claveConfirm){
                 $_SESSION['mensaje'] = "Las contraseñas deben coincidir";
@@ -48,14 +41,9 @@ if (!empty($_POST['button_upd'])){
                 header("location: ../../paciente/editarPerfil/editarClave.php");
             }
             else{
-                // echo "si";
-                // //HACER REGISTRO EN BASE DE DATOS
-                // include 'conexion.php'; //Conexión con base de datos
-
                 $clave= md5($clave);
 
                 $consulta = "UPDATE cuentas SET clave = '$clave' WHERE id_cuenta = '$id'";
-                // echo $consulta;
                 $query = mysqli_query($conexion, $consulta);
 
                 if ($query){
