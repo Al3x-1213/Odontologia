15884e0a6e3d9623339b6f21553f7e5819fe0018
VeronicaTrias <gitygithub@gmail.com>
2023-09-29 22:28:49 -0400

    cambios y conflictos

--- admin/components/index.php
+++ admin/components/index.php
@@ -1,9 +1,3 @@
 <?php
-<<<<<<< HEAD
 header ("location: ../../index.php");
-=======
-
-header ("location: ../../index.php");
-
->>>>>>> 9b9d086bcecced9c3d0cd6cc524b8edad033f730
 ?>
--- paciente/editarPerfil/index.php
+++ paciente/editarPerfil/index.php
@@ -1,9 +1,3 @@
 <?php
-
-<<<<<<< HEAD
-include '../client/verificationSessionAdmin.php';
-=======
 header ("location: ../../index.php");
->>>>>>> 9b9d086bcecced9c3d0cd6cc524b8edad033f730
-
 ?>
